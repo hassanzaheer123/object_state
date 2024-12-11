503757b6a4c4ac2e84b3621bbcb88ca3d264897f
Hassan Zaheer <hassan.zaheer.abbasi@gmail.com>
2024-12-11 23:50:59 +0500

    new msg

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,33 @@ const App = () => {
   return (
     <>
       <div className="container">
-        <h1>Welcome to the React App</h1>
-        <p>This is a simple React app.</p>
+        <div className="row mt-4">
+          <div className="col-lg-4 mx-auto p-3">
+            <h1 className='text-center'>Multiple Controlled Inputs</h1>
+            <form className='form border rounded p-3'>
+              <div className="mt-2">
+                <label htmlFor="" className="">Name :</label>
+                <input type="text" className='form-control mt-1' placeholder='Please Enter Your Name' />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="" className="">Email :</label>
+                <input type="email" className='form-control mt-1' placeholder='Please Enter Your Email' />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="" className="">Age :</label>
+                <input type="number" className='form-control mt-1' placeholder='Please Enter Your Age' />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="" className="">Date Of Birth :</label>
+                <input type="date" className='form-control mt-1' placeholder='Please Enter Your Date Of Birth' />
+              </div>
+              <div className="mt-2">
+                <label htmlFor="" className="">Address :</label>
+                <textarea type="text" className='form-control mt-1' placeholder='Please Enter Your Address' />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   )
