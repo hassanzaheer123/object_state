82d5650fa50156eaf4038502c014cc95c2162ff5
Hassan Zaheer <hassan.zaheer.abbasi@gmail.com>
2024-12-11 23:42:22 +0500

    object state

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>Welcome to the React App</h1>
+        <p>This is a simple React app.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
